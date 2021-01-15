41b1050ca351ac073e93385fe3d39bce6671e8e3
moeinrahimi <imoeinrahimi1@@gmail.com>
2021-01-15 15:03:36 +0330

    fix counter

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
         let count = page.visitCount.toString();
         if (count > 10000) count = ':D';
         chrome.browserAction.setIcon({ path: 'images/blue-64.png', tabId: tabId });
-        chrome.browserAction.setBadgeText({ text: 'count', tabId: tabId });
+        chrome.browserAction.setBadgeText({ text: count, tabId: tabId });
         return;
       }
     });
